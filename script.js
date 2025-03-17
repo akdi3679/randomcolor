9dd55cdde6ce178f55d5d6a524cbe17d15d33831
akdi3679 <3kadiak679@gmail.com>
2025-03-17 13:30:44 +0100

    randommco

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Attendre que le DOM soit complètement chargé
 document.addEventListener('DOMContentLoaded', () => {
     const colorBox = document.getElementById('color-box');
     const changeColorBtn = document.getElementById('change-color-btn');
   
-    // Fonction pour générer une couleur hexadécimale aléatoire
     function getRandomColor() {
       const letters = '0123456789ABCDEF';
       let color = '#';
@@ -13,9 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
       return color;
     }
   
-    // Ajouter un écouteur d'événements sur le bouton pour changer la couleur
     changeColorBtn.addEventListener('click', () => {
-      // Changer la couleur de fond de la boîte
       const newColor = getRandomColor();
       colorBox.style.backgroundColor = newColor;
     });
